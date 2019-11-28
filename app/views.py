ba0e9119fae5608bc38da143ec5c7024ab0433e3
yamadaitsuki <yamada351035@gmail.com>
2019-11-28 19:36:13 +0900

    ADD:signup機能
    - signupページからユーザー登録し、正しく登録されていればログインしてユーザーページに遷移する
    ```
    login メソッド自体は認証機能を備えていないため、その前に authenticate メソッドで 認証を行なっています。
    ```

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Photo
 
@@ -19,5 +20,16 @@ def users_detail(request, pk):
 
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            input_username = form.cleaned_data['username']
+            input_password = form.cleaned_data['password1']
+            new_user = authenticate(username=input_username, password=input_password)
+            if new_user is not None:
+                login(request, new_user)
+                return redirect('app:users_detail', pk=new_user.pk)
+    else:
+        form = UserCreationForm()
     return render(request, 'app/signup.html', {'form': form})
