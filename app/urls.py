aade728f4e71a7ce6cb32a79a54315fe288e0124
yamadaitsuki <yamada351035@gmail.com>
2019-11-28 19:24:32 +0900

    ADD:signupページ雛形
    - 標準のsignup機能を使用
    - django.contrib.auth.forms.UserCreationForm

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('users/<int:pk>', views.users_detail, name='users_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404
 
 from .models import Photo
@@ -15,3 +16,8 @@ def users_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
     photos = user.photo_set.all().order_by('-created_at')
     return render(request, 'app/users_detail.html', {'user': user, 'photos': photos})
+
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'app/signup.html', {'form': form})
