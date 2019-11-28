2444b169f4deb45194cc878229900107770c2300
yamadaitsuki <yamada351035@gmail.com>
2019-11-28 17:12:12 +0900

    ADD:login/logout機能追加

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from app import views
@@ -6,4 +7,6 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('users/<int:pk>', views.users_detail, name='users_detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
